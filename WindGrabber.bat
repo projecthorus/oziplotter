1dd21f6d2bf205e0d055fd9d39f31f70e3fe8c6a
Mark Jessop <vk5qi@rfhead.net>
2018-02-21 20:11:13 +1030

    Update get_wind_data to new GRIB downloader version.

--- WindGrabber.bat
+++ WindGrabber.bat
@@ -4,6 +4,6 @@ CLS
 echo ------------------------------------------------------------
 echo DON'T FORGET TO DELETE OLD WIND DATA FROM THE GFS DIRECTORY!
 echo ------------------------------------------------------------
-python src/get_wind_data.py --lat=-33 --lon=139 --latdelta=10 --londelta=10 -v -f 24 -r 0p50
+python src/get_wind_data.py --lat=-33 --lon=139 --latdelta=10 --londelta=10 -f 24 -m 0p25_1hr
 echo DON'T FORGET TO DELETE OLD WIND DATA FROM THE GFS DIRECTORY!
 pause
